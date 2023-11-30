f7e4fa18cb6deb192f42a38f45fab30802ccd89e
Tawhan Studio <36127485+tawhan695@users.noreply.github.com>
2023-11-30 10:31:57 +0700

    update

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 cd ~
 echo 3 > /proc/sys/vm/drop_caches
 cp test-bot/data.json data.json.backup
-cp test-bot/token.txt token.txt.backup
+# cp test-bot/token.txt token.txt.backup
 rm -r test-bot
 git clone https://github.com/tawhan695/test-bot.git
 cp data.json.backup test-bot/data.json
-cp token.txt.backup test-bot/token.txt
+# cp token.txt.backup test-bot/token.txt
 cd test-bot
 go build main.go
 cd ~
